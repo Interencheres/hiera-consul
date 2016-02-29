0f7e5ca2e343bce778904b1d5d217f6e6da70e43
Olivier <ofontannaud@cpmultimedia.com>
2016-02-29 11:56:07 +0100

    feat: return all responses

--- lib/hiera/backend/consul_backend.rb
+++ lib/hiera/backend/consul_backend.rb
@@ -92,7 +92,7 @@ class Hiera
           else
             Hiera.debug("[hiera-consul]: Jumped as array empty")
           end
-          Hiera.debug(answer)
+          Hiera.debug("[hiera-consul] machin: + #{answer})
           return answer
       end
 
